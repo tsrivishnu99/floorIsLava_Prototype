2fcc4947b9e8bb1e7ca1c596c16e625b22a3f56a
Srinivasan <tsrivishnu99@gmail.com>
2015-09-13 01:55:18 -0400

    stop 1

---
+++ Assets/Scripts/player_movementHandler_v1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_movementHandler_v1 : MonoBehaviour {
+
+    private CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+        characterController = this.gameObject.GetComponent<CharacterController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
